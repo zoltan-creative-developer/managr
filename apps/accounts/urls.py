c2786a97f76e9e0c79dc3a2abb8cd9885706706d
Kelecsényi Zoli <zoltan.kelecsenyi@gmail.com>
2025-09-26 10:37:56 +0200

    Employee login finished

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.accounts.views import management_login_view, management_dashboard_view
+from apps.accounts.views import management_login_view, management_dashboard_view, employee_login_view, employee_dashboard_view
 
 urlpatterns = [
     path('management-login/', management_login_view, name='management_login'),
     path('management/dashboard/', management_dashboard_view, name='management_dashboard'),
+    path('login/', employee_login_view, name='employee_login'),
+    path('employee/', employee_dashboard_view, name='employee_dashboard'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -25,3 +25,28 @@ def management_dashboard_view(request):
         'welcome_message': 'Welcome to the management dashboard.',
     }
     return render(request, 'apps/accounts/templates/accounts/management-dashboard.html', context)
+
+def employee_login_view(request):
+    """Custom employee login view that renders the employee_login.html template"""
+    context = {
+        'title': 'Employee Login',
+        'welcome_message': 'Add a valid email and password to log in.',
+    }
+    if request.method == 'POST':
+        received_email = request.POST.get('user_email')
+        received_password = request.POST.get('password')
+        user = authenticate(request, email=received_email, password=received_password)
+        if user is not None and not user.is_staff:  # Check if user is not admin/staff
+            login(request, user)
+            return redirect('employee_dashboard')  # Redirect to custom employee dashboard after login
+        else:
+            return render(request, 'apps/accounts/templates/accounts/employee-login.html', {'error': 'Wrong email or password. You are not authorized to access this page.'})
+    return render(request, 'apps/accounts/templates/accounts/employee-login.html')
+
+def employee_dashboard_view(request):
+    """Custom employee dashboard view that renders the employee_dashboard.html template"""
+    context = {
+        'title': 'Employee Dashboard',
+        'welcome_message': 'Welcome to the employee dashboard.',
+    }
+    return render(request, 'apps/accounts/templates/accounts/employee-dashboard.html', context)
